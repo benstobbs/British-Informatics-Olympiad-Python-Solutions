6314bcad33b1f40c98798836fe7f564d489d7cf1
Ben Stobbs <ben@stobbs.com>
2017-07-06 19:38:20 +0100

    Revert "Added 2017 Q1 - Colour Triangles"
    This reverts commit f486a5d20c6da4cd806611e9adaa7bd95108297f.

--- BIO16/Q3 - Prime Connections.py
+++
@@ -1,16 +0,0 @@
-inp = input(">").split(" ")
-l,p,q = int(inp[0]),int(inp[1]),int(inp[2])
-def genprimes(n): 
-    limitn = n+1 
-    not_prime = set() 
-    primes = [] 
- 
-    for i in range(2, limitn): 
-        if i in not_prime: 
-            continue 
-        for f in range(i*2, limitn, i): 
-            not_prime.add(f) 
-        primes.append(i) 
-    return primes
-
-primes = genprimes(l)
--- BIO17/Q2 - Dots.py
+++
@@ -1,5 +0,0 @@
-grid = [[0 for y in range(5)] for x in range(5)]
-
-
-         
-print(grid)
